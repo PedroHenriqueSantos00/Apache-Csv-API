17e1b62984d56f15341c859b3d4d43f7aa466771
PHS <Phenriques1321@gmail.com>
2020-03-26 19:42:03 -0300

    veiculo e persistenciaVeiculos completos

---
+++ Veiculo.java
@@ -0,0 +1,34 @@
+public class Veiculo{
+    private String placa;
+    private String marca;
+    private String cor;
+    private String categoria;
+
+    public Veiculo(String placa, String marca, String cor, String categoria){
+        this.placa = placa;
+        this.marca = marca;
+        this.cor = cor;
+        this.categoria = categoria;
+    }
+    
+
+    public String getPlaca(){
+        return placa;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public String getCor(){
+        return cor;
+    }
+
+    public String getCategoria(){
+        return categoria;
+    }
+
+    public String toString(){
+        return "[placa: " + placa + ", marca: " + marca + ", cor: " + cor + ", categoria: " + categoria + "]";
+    }
+}
