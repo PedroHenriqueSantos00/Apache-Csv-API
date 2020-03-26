0a9aafe03c956e04109a3c89cf72f074ae934a78
VitaoDoEspadao <victor.huander@outlook.com>
2020-03-26 11:47:57 -0300

    atualizacao

--- Motorista.java
+++ Motorista.java
@@ -1,4 +1,33 @@
 public class Motorista{
 
+ private int cpf;
+ private String nome;
+ private Veiculo veiculo;
+ private FormaPagamento pagamaneto;
+
+    public Motorista(int cpf, String nome, Veiculo veiculo, FormaPagamento pagamento){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.veiculo = veiculo;
+        this.pagamento = pagamento;
+
+    }
+
+    public int getCPF(int cpf){
+        return cpf;
+    }
+
+    public String getNome(String nome){
+        return nome;
+    }
+
+    public Veiculo getVeiculo(Veiculo veiculo){
+        return veiculo;
+    }
+
+    public FormaPagamento pagamento(FormaPagamento pagamento){
+        return pagamento;
+    }
+
     
 }
